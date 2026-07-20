d0214425b33755a7c386dbe897456efa460b57cc
FrancescoIngv <FrancescoIngv@users.noreply.github.com>
2026-07-20 10:13:00 +0200

    fixed pybox.remove_resource()

--- expose_plugins/process/pybox.py
+++ expose_plugins/process/pybox.py
@@ -1051,19 +1051,31 @@ class PyboxProcessor(BaseRemoteExecutionProcessorLocalReference):
         return code_input_param
 
     def remove_resources(self, job_id: str) -> None:
-        resource_file = self.base_reference_path / f"{job_id}_input_data.txt"
+        resource_file = self.base_reference_path / f'{job_id}_input_data.txt'
         resource_file.unlink(missing_ok=True)
 
-        resource_file = self.base_reference_path / f"{job_id}_dem.json"
+        resource_file = self.base_reference_path / f'{job_id}_dem.json'
         resource_file.unlink(missing_ok=True)
 
-        resource_file = self.base_reference_path / f"{job_id}_invasion_map.json"
+        resource_file = self.base_reference_path / f'{job_id}_dem.tif'
         resource_file.unlink(missing_ok=True)
 
-        resource_file = self.base_reference_path / f"{job_id}_spatial_evolution.json"
+        resource_file = self.base_reference_path / f'{job_id}_dem.sld'
         resource_file.unlink(missing_ok=True)
 
-        resource_file = self.base_reference_path / f"{job_id}_deposit_thickness.json"
+        resource_file = self.base_reference_path / f'{job_id}_invasion_map.json'
+        resource_file.unlink(missing_ok=True)
+
+        resource_file = self.base_reference_path / f'{job_id}_invasion_map.tif'
+        resource_file.unlink(missing_ok=True)
+
+        resource_file = self.base_reference_path / f'{job_id}_invasion_map.sld'
+        resource_file.unlink(missing_ok=True)
+
+        resource_file = self.base_reference_path / f'{job_id}_spatial_evolution.json'
+        resource_file.unlink(missing_ok=True)
+
+        resource_file = self.base_reference_path / f'{job_id}_deposit_thickness.json'
         resource_file.unlink(missing_ok=True)
 
     def __repr__(self):
